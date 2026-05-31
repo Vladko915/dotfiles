50e3b9809fb14a2681ac501085220363d06d1f47
Vladko915 <88051272+Vladko915@users.noreply.github.com>
2026-05-31 13:50:33 +0300

    Implement SSH key loading with ssh-agent
    Add SSH agent initialization and key loading to script

--- bash/70-secure.sh
+++ bash/70-secure.sh
@@ -1,7 +1,24 @@
+#ssh
+
+#idempotent call
+if command -v ssh-agent >/dev/null 2>&1 && command -v ssh-add >/dev/null 2>&1; then
+    alias inkey='
+        if ! ssh-add -l >/dev/null 2>&1; then
+            eval $(ssh-agent -s)
+            ssh-add ~/.ssh/id_ed25519
+            echo "SSH key successfully loaded"
+        else
+            echo "SSH key already active"
+        fi
+    '
+fi
+
 if command -v snyk >/dev/null 2>&1; then
 
     alias snk="snyk test"
 
 fi
+
+
 
 
