20c12f6b1555d4c066a866daff3c692ce4e470c3
Vladko915 <88051272+Vladko915@users.noreply.github.com>
2026-05-31 14:39:49 +0300

    Add arunsh function for executing shell commands

--- bash/55-ansible.sh
+++ bash/55-ansible.sh
@@ -3,3 +3,10 @@ if command -v ansible >/dev/null 2>&1; then
         ansible -i ~/hosts.ini kubernetes -m command -a "$1" | awk '/CHANGED/{print "----------"}1'
     }
 fi
+
+#WARNING
+if command -v ansible >/dev/null 2>&1; then
+    arunsh() {
+        ansible -i ~/hosts.ini kubernetes -m shell -a "$1" | awk '/CHANGED/{print "--------------------"}1'
+    }
+fi
