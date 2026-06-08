73a52789a0a96c8194b48f515eb6a1ac82106208
Vladko915 <vladvladko55@gmail.com>
2026-06-09 00:42:23 +0300

    Added alias 'kgtoken' to bash/50-kubectl.sh

--- bash/50-kubectl.sh
+++ bash/50-kubectl.sh
@@ -4,10 +4,18 @@ if command -v kubectl >/dev/null 2>&1; then
   alias kgp="kubectl get pods"
   alias kgd="kubectl get deployments"
   alias kgs="kubectl get services"
+  alias kgtoken="kubeadm token create --print-join-command"
 
   ksd ()
   {
     kubectl scale deployments "$1" --replicas="$2"
   }
+
+fi
+
+#kubeadm
+if command -v kubeadm >/dev/null 2>&1; then
+
+  alias kgtoken="kubeadm token create --print-join-command"
 
 fi
